6c4fbb3dd90092e6f9a96180930d747d03eb0b8f
tanner.chung@gmail.com <tanner.chung@gmail.com>
2025-08-25 08:28:30 -0400

    syntax

--- src/components/MultiAgentDemo.js
+++ src/components/MultiAgentDemo.js
@@ -159,7 +159,7 @@ const MessageContent = ({ content }) => {
         tr: ({ children }) => <tr>{children}</tr>,
         th: ({ children }) => <th className="px-3 py-2 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">{children}</th>,
         td: ({ children }) => <td className="px-3 py-2 text-sm text-gray-900">{children}</td>,
-        }}
+      }}
       >
         {processedContent}
       </ReactMarkdown>
@@ -1605,7 +1605,7 @@ const MultiAgentDemo = () => {
             )}
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
